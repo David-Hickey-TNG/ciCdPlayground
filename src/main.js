86a9abea2f93b70ef3e52ae7f9bacf7c664caaca
David-Hickey-TNG <164214950+David-Hickey-TNG@users.noreply.github.com>
2024-05-03 13:49:48 +0200

    Update my deets

--- src/main.js
+++ src/main.js
@@ -4,25 +4,27 @@ new App({
 	target: document.body,
 	props: {
 		// What's your name?
-		name: 'James Butler',
+		name: 'David',
 		// In the following fiels you can either give a single string, 
 		// or an array of bullet points
 		
 		// What do you associate with the term 'CI/CD'?
 		associations: [
-			'Ehm, what?'
+			'Taking forever to run',
+			'Strange error messages in my email inbox like THERE WERE NO FAILURES'
 		],
 		// Which CI/CD tools do you use in your project?
-		tools: 'Jenkins, maybe?',
+		tools: 'Jenkins, Bitbucket?',
 		// What do you want to learn in this workshop? 
 		expectations: [
-			'what you are talking about'
+			'How the stuff I\'m using works'
 		],
 		// What do you like to do when you're not coding?
 		hobbies: [
-			'play chess', 
-			'do skydiving', 
-			'do my taxes'
+			'running', 
+			'baking', 
+			'bouldering',
+			'judo (just started)'
 		]
 	}
 });
